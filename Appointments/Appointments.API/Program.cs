50c0ad7464181675861b2b999967e141e00ba3d7
ajmarrugos <ajmarrugos@gmail.com>
2024-11-07 10:16:39 -0500

    Implemented loading sample appoinments from json file

--- Appointments/Appointments.API/Program.cs
+++ Appointments/Appointments.API/Program.cs
@@ -7,6 +7,18 @@ var app = builder.Build();
 
 var appointments = new List<Appointment>();
 
+string samples = "appointments.json";
+
+if (File.Exists(samples))
+{
+    var jsonData = File.ReadAllText(samples);
+    var loaded = JsonSerializer.Deserialize<List<Appointment>>(jsonData);
+    if (loaded != null)
+    {
+        appointments.AddRange(loaded);
+    }
+}
+
 app.MapGet("/appointments", () =>
 {
     return Results.Ok(appointments);
