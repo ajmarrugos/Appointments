0bfd472899b4f5878b4bbbbbaf764f3bee9a0510
ajmarrugos <ajmarrugos@gmail.com>
2024-11-07 10:56:29 -0500

    Switched to MVC pattern

---
+++ Appointments/Appointments.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Appointments.API.Models;
+
+namespace Appointments.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        private static readonly List<Appointment> Appointments = new();
+
+        [HttpGet]
+        public IActionResult GetAppointments()
+        {
+            return Ok(Appointments);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAppointmentById(Guid id)
+        {
+            var appointment = Appointments.FirstOrDefault(a => a.Id == id);
+            return appointment is not null ? Ok(appointment) : NotFound();
+        }
+    }
+}
