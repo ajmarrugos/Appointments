06a2afaa5e1d6240aee4bfb01a772d823744cae0
ajmarrugos <ajmarrugos@gmail.com>
2024-11-08 10:06:32 -0500

    Implementing ConfigureServices in main program

--- Appointments/Appointments.API/Program.cs
+++ Appointments/Appointments.API/Program.cs
@@ -1,12 +1,11 @@
-using Appointments.API;
-using Appointments.API.Repository;
+using Microsoft.EntityFrameworkCore;
+using Appointments.API.Repositories;
+using Appointments.API.Interfaces;
+using Appointments.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddSingleton<IRepository, LocalRepository>();
-builder.Services.AddOutputCache(opt => 
-    opt.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(30));
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,3 +14,21 @@ app.UseOutputCache();
 app.MapControllers();
 
 app.Run();
+
+void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+{
+    // Register Controllers
+    services.AddControllers();
+
+    // Registers OutputCache for speeding endpoint response time
+    builder.Services.AddOutputCache(opt =>
+        opt.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(30));
+
+    // Register repositories as scoped services
+    services.AddSingleton<IAppointmentsRepository, AppointmentsDB>(); // Using the SQL Database repository
+    // services.AddSingleton<IAppointmentsRepository, LocalRepository>(); // Using the Local Repository
+
+    // Register the DbContext with dependency injection
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentsDb")));
+}
