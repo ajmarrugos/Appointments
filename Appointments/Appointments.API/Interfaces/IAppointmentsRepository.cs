b706c2334146854fcc0ca64f27603ed6dd244533
ajmarrugos <ajmarrugos@gmail.com>
2024-11-08 08:41:21 -0500

    Proper naming and location for IAppointmentsRepository

---
+++ Appointments/Appointments.API/Interfaces/IAppointmentsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Appointments.API.Models;
+
+namespace Appointments.API.Interfaces
+{
+    public interface IAppointmentsRepository
+    {
+        IEnumerable<Appointment> GetAllAppointments();
+        Appointment? GetAppointmentById(Guid id);
+        IEnumerable<Appointment> GetAppointmentsBySender(string senderEmail);
+        Appointment CreateAppointment(Appointment appointment);
+        Appointment? RescheduleAppointment(Guid id, DateTime newDate);
+        Appointment? SignoffAppointment(Guid id, AppointmentStatus status);
+        bool DeleteAppointment(Guid id);
+    }
+}
