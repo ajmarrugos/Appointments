22b7e57ec720c7ca6078ae3bda48b57658bc27da
ajmarrugos <ajmarrugos@gmail.com>
2024-11-11 07:44:54 -0500

    New User class in the flow

---
+++ Appointments/Appointments.API/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appointments.API.Models
+{
+    public class User
+    {
+        [Key]
+        public string Identifier { get; set; } // IP or Email
+
+        [Required]
+        public string Role { get; set; } // "Manager", "User"
+    }
+}
