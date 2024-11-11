1aa60c52acf7b7cce55f5c19d70a7b0421fffaeb
ajmarrugos <ajmarrugos@gmail.com>
2024-11-11 10:35:54 -0500

    Fixed comments for the AppDbContext

--- Appointments/Appointments.API/Data/AppDbContext .cs
+++ Appointments/Appointments.API/Data/AppDbContext .cs
@@ -5,16 +5,15 @@ namespace Appointments.API.Data
 {
     public class AppDbContext : DbContext
     {
-        // Defines a DbSet properties for each entity in your database
         public DbSet<Appointment> Appointments { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        // Optionally override OnModelCreating to configure model relationships
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-            // Additional model configuration can be added here
+            modelBuilder.Entity<Appointment>()
+                .ToTable("Appointments")
+                .HasKey(a => a.Id);
         }
     }
 }
